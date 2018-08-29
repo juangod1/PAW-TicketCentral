89e5157158790ea4665ef0a7932819fb47e4665c
juangod1 <juan.godfrid@gmail.com>
2018-08-29 20:31:22 -0300

    alto font

--- webApp/src/main/java/ar/edu/itba/paw2018b/config/WebConfig.java
+++ webApp/src/main/java/ar/edu/itba/paw2018b/config/WebConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
@@ -14,7 +16,7 @@ import javax.sql.DataSource;
 @EnableWebMvc
 @ComponentScan({ "ar.edu.itba.paw2018b.controller", "ar.edu.itba.paw2018b.services","ar.edu.itba.paw2018b.persistence" })
 @Configuration
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurerAdapter {
     @Bean
     public ViewResolver viewResolver() {
         final InternalResourceViewResolver viewResolver =
@@ -23,6 +25,12 @@ public class WebConfig {
         viewResolver.setPrefix("/WEB-INF/jsp/");
         viewResolver.setSuffix(".jsp");
         return viewResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
     }
 
     @Bean
--- webApp/src/main/webapp/WEB-INF/jsp/index.jsp
+++ webApp/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -1,11 +1,11 @@
 <%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
-<style><%@include file="/WEB-INF/assets/css/style.css"%></style>
-
 
 <html>
     <head>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="<c:url value="/resources/css/style.css"/>"/>
+
     </head>
     <body>
         <div class="background">
