c7d62b17139c20c42ee1b69415cd1fdaa577b5d6
pabloito <pradnic@itba.edu.ar>
2018-09-18 22:22:04 -0300

    fix

--- webApp/src/main/webapp/resources/js/admin.js
+++ webApp/src/main/webapp/resources/js/admin.js
@@ -14,7 +14,10 @@ function executeQuery() {
 
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
+        if(this.readyState!=4){
+            return;
+        }
+        if (this.status == 200) {
             var transactions = JSON.parse(this.responseText);
             buildTransactions(transactions);
         }
